bca919b1b81fe1c6110ca268ecb9e39af0b9753b
Oziel Junior <ozieljr14@gmail.com>
2023-02-13 17:08:15 -0400

    att

--- python_project/ajusta_imagem_apostadores.py
+++ python_project/ajusta_imagem_apostadores.py
@@ -1,8 +1,9 @@
 import os
 import cv2
-import pytesseract
+from pytesseract import pytesseract
 from matplotlib import pyplot as plt
 from openpyxl import Workbook
+import numpy as np
     
 wb = Workbook()
 ws = wb.active
@@ -12,18 +13,24 @@ indice_ex = 1
 i = 0 
 odd_original = []
 
-while i <= 250:
-    print("Imagem: ", i)
+#C:\Program Files\Tesseract-OCR\tesseract.exe
+pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+while i <= 500:
+    #print("Imagem: ", i)
     name = str(i)+"T_Apostadores.png"
-    name_path = "/home/oziel/Documentos/Personal_project/Aviator/Print_de_telas/data/qt_apostadores/" + name
+    #/home/oziel/Documentos/Personal_project/Aviator/Print_de_telas/data/qt_apostadores/
+    name_path = "C:/Users/Riallen/Documents/Print_de_telas/data1/qt_apostadores/" + name
     image = cv2.imread(name_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur =cv2.GaussianBlur(gray, (1,1), 0)
+    kernel = np.ones((1,1),np.float32)/25
+    dst = cv2.filter2D(gray,-1,kernel)
+    blur =cv2.GaussianBlur(dst, (1,1), 0)
     thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     
-    plt.imshow(thresh),plt.show()
-    
-    text = pytesseract.image_to_string(thresh, config = "--psm 6")
+    #plt.imshow(thresh),plt.show()
+    custom_config = r'--oem 3 --psm 6 outputbase digits'
+    text = pytesseract.image_to_string(thresh, config = custom_config)
     #print("Odd: ", text)
     
     odd_original.append(text)
@@ -34,29 +41,32 @@ print(odd_original)
 print(len(odd_original))
 
 # =============================================================================
-odd_editada = []
-bad = ["\n\x0c"]
+# # =============================================================================
+apostadores_editada = []
+bad = ["\n", '.']
 for name in odd_original:
-     for i in bad:
-         name = name.replace(i,"")
-     a = name.replace(",","")
-     b = a.replace("V71","1771")
-     odd_editada.append(int(b))
-# 
-print(odd_editada)
-print(len(odd_editada))
-# 
+    for i in bad:
+        name = name.replace(i,"")
+#      a = name.replace(",","")
+#      b = a.replace("V71","1771")
+    apostadores_editada.append(int(name))
+# # 
+print(apostadores_editada)
+print(len(apostadores_editada))
+# # 
 i = 0
-# 
-while i <= 250:
+# # 
+while i <= 500:
     ap = 'A' + str(indice_ex)
-    ws[ap] = odd_editada[i]
-#     
+    ws[ap] = apostadores_editada[i]
+# #     
     indice_ex += 1
     i += 1
-name = '/home/oziel/Documentos/Personal_project/Aviator/Print_de_telas/qt_apostadores.xlsx'  
+#/home/oziel/Documentos/Personal_project/Aviator/Print_de_telas/
+name = 'C:\\Users\\Riallen\\Documents\\Print_de_telas\\qt_apostadores1.xlsx'  
 wb.save(name)
-#     
+# #     
+# =============================================================================
 # =============================================================================
     
     
