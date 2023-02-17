376a29d3b032a0918094ba73775053932b3d0281
Oziel Junior <ozieljr14@gmail.com>
2023-02-17 00:52:50 -0400

    att

---
+++ accounts/settings.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+
+
+settings.SESSION_COOKIE_SECURE = True
+settings.CSRF_COOKIE_SECURE = True
+settings.SECURE_BROWSER_XSS_FILTER = True
+settings.SECURE_CONTENT_TYPE_NOSNIFF = True
+settings.SECURE_SSL_REDIRECT = True
+settings.X_FRAME_OPTIONS = 'DENY'
---
+++ accounts/views.py
@@ -0,0 +1,59 @@
+# Create your views here.
+
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from .models import Profile, System_Post
+from .forms import SystemForm, CustomUserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.contrib import messages
+
+
+@login_required
+def dashboard(request):
+    if request.method == "POST":
+        form = SystemForm(request.POST or None)
+        if form.is_valid():
+            system = form.save(commit=False)
+            system.user = request.user
+            system.save()
+            return redirect("System_Post:dashboard")
+    form = SystemForm()
+    followed_pots = System_Post.objects.filter(user__profile__in=request.user.profile.follows.all()).order_by(
+        "-created_at")
+    return render(request, "system_list/dashboard.html", {"form": form, "post": followed_pots})
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "system_list/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+    profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()
+    return render(request, "system_list/profile.html", {"profile": profile})
+
+
+def register(request):
+    if request.method=='POST':
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Seu registro foi criado com sucesso!')
+            return HttpResponseRedirect(reverse('login'))
+    else:
+        form=CustomUserCreationForm()
+    args={'form':form}
+    return render(request,'system_list/register.html',args)
